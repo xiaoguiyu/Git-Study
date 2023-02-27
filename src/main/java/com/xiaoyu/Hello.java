71d98707b82a456d195543abb904b061c40d1965
xiaoyu <lin802366@gmial.com>
2023-02-27 18:43:32 +0800

    添加了hello, world 代码

---
+++ src/main/java/com/xiaoyu/Hello.java
@@ -0,0 +1,21 @@
+package com.xiaoyu;
+
+/**
+ * @author xiaoyu
+ * @version 1.0
+ * @date 2023/2/27 18:40
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("hello, world!");
+        System.out.println("hello, world!");
+        System.out.println("hello, world!");
+        System.out.println("hello, world!");
+        System.out.println("hello, world!");
+
+
+
+    }
+}
