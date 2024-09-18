3b65b5dd96573117dddd28cf417a49a4da6c225c
xiaoyu <lin802366@gmail.com>
2024-09-18 14:21:20 +0800

    删除java

--- Test.java
+++ Test.java
@@ -1,6 +1,9 @@
 public class Test{
 	public static void main(String[] args) {
 		System.out.print("hello, world!");
+		System.out.print("hello, world!");
+		System.out.print("hello, world!");
+		System.out.print("hello, world!");
 	}
 
 
